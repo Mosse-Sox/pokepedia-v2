0a052da09ceb82594ccd7bcff8371c9fbccfeedc
Mosse-Sox <autumngreenie990@gmail.com>
2023-10-11 15:47:59 -0600

    adds prisma service to backend

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,14 +3,17 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'client', 'dist'),
     }),
+    PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
